4418b718becf62ba06db1dbfa42a7b8d685d0cbc
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2017-12-18 22:17:14 -0200

    Teste DigitSum

--- src/challenges/hackerrank/DigitSum.java
+++ src/challenges/hackerrank/DigitSum.java
@@ -6,22 +6,41 @@ public class DigitSum {
 	
 	// https://www.hackerrank.com/challenges/recursive-digit-sum/problem
 
-	static int superDigit(String n, int k) {
+	static Long superDigit(String n, int k) {
         // Complete this function
-		StringBuilder pString = new StringBuilder();
+		
+		char[] charArray = n.toCharArray();
+		//System.out.println("Tamanho de N: " + charArray.length);
+		//return (long) charArray.length;
+				
+		int sizeOfArray = (n.toCharArray().length * k);
+		//System.out.println("Tamanho total do array: " + sizeOfArray);
+		char[] pArray = new char[sizeOfArray];
+		
+		
+		int count = 0;
+		Long sum = 0L;
 		for (int i = 0; i < k ; i++) {
-			pString.append(n);
+			for (int y = 0; y < charArray.length; y++) {
+				//System.out.println("Na posição " + count + " o valor é: " + charArray[y]);
+				pArray[count] = charArray[y];
+				sum = (sum + Character.getNumericValue(charArray[y]));
+				count++;
+			}
 		}
-		Integer result = super_digit(pString.toString());
+
+		//System.out.println("Array final: " + Arrays.toString(pArray));
+
+		Long result = super_digit(sum.toString());
 		while (result > 10) {
 			result = super_digit(result.toString());
 		}
 		return result;
     }
 
-	private static int super_digit(String p) {
+	private static Long super_digit(String p) {
 		char[] charArray = p.toCharArray();
-		int total = 0;
+		Long total = 0L;
 		for (int y = 0; y < charArray.length; y++) {
 			int currentValue = Character.getNumericValue(charArray[y]);
 			total = (total + currentValue);
@@ -34,7 +53,7 @@ public class DigitSum {
         Scanner in = new Scanner(System.in);
         String n = in.next();
         int k = in.nextInt();
-        int result = superDigit(n, k);
+        Long result = superDigit(n, k);
         System.out.println(result);
         in.close();
     }
