46f5c8eeaee060a9c04bf4c6679419753a608cf2
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2018-01-03 22:34:49 -0200

    Add solution for Most Common Element In Array

---
+++ src/challenges/hackerrank/MostCommonElementInArray.java
@@ -0,0 +1,50 @@
+package challenges.hackerrank;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class MostCommonElementInArray {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] numbers = new int[n];
+		for (int i = 0; i < n; i++) {
+			numbers[i] = in.nextInt();
+		}
+		/* 
+		 	Example of input:
+
+			6
+			1 3 1 3 2 1
+			
+			Example of output:
+
+			1
+
+		*/
+		System.out.println(most_frequent(numbers));
+	}
+
+	static int most_frequent(int given_array[]) {
+		Integer result = null;
+		Arrays.sort(given_array);
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < given_array.length; i++) {
+			if (map.containsKey(given_array[i])) {
+				map.put(given_array[i], (map.get(given_array[i]) + 1));
+			} else  {
+				map.put(given_array[i], 1);			
+			}
+		}
+		Entry<Integer, Integer> entry = map.entrySet().stream()
+			.sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+			.findFirst().get();
+		
+		//System.out.println("The key " + entry.getKey() + " appears " + entry.getValue() + " times.");
+		return entry.getKey();
+	}
+}
