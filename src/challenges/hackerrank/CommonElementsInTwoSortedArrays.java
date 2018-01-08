cd81253887d31659efc655954a6a910d224199cf
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2018-01-08 20:58:20 -0200

    Add examples of CommonElementsInTwoSortedArrays

--- src/challenges/hackerrank/CommonElementsInTwoSortedArrays.java
+++ src/challenges/hackerrank/CommonElementsInTwoSortedArrays.java
@@ -11,11 +11,14 @@ public class CommonElementsInTwoSortedArrays {
 		Integer[] a = {1,3,4,6,7,9};
 		Integer[] b = {1,2,4,5,9,10};
 		Integer result[] = common_elemens(a, b);
-		
+		System.out.println(Arrays.toString(result));
+		// result must be: [1,4,9]
+
 		Integer[] array1B = {1, 2, 9, 10, 11, 12};
         Integer[] array2B = {0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15};
         Integer resultb[] = common_elemens(array1B, array2B);
-		//int result[] = {1,4,9};
+        System.out.println(Arrays.toString(resultb));
+		// result must be: [1, 2, 9, 10, 12]
 	}
 
 	private static Integer[] common_elemens(Integer[] a, Integer[] b) {
@@ -28,7 +31,6 @@ public class CommonElementsInTwoSortedArrays {
 				result.add(item);
 			}
 		}
-		System.out.println(result.toString());
 		Integer[] resultArray = new Integer[result.size()];
  		return result.toArray(resultArray);
 	} 
